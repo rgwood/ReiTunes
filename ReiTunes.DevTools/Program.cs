1b9b0f597f6ef40fb373140d84a6747c3d579093
Reilly Wood <reilly.wood@icloud.com>
2020-05-24 16:38:22 -0700

    Add devtools project

---
+++ ReiTunes.DevTools/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace ReiTunes.DevTools
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // setx AZURE_STORAGE_CONNECTION_STRING "<yourconnectionstring>"
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            await ListBlobs(connectionString);
+        }
+
+        static async Task ListBlobs(string connectionString)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("reitunes");
+
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine(blobItem.Name);
+            }
+        }
+
+    }
+}
