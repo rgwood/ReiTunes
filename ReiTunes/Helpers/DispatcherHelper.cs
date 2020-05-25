0aeeeb3c2dca683180a6d4ec360f6e4f517c1579
Reilly Wood <reilly.wood@icloud.com>
2020-05-25 16:40:40 -0700

    Add dispatcher helper

---
+++ ReiTunes/Helpers/DispatcherHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace ReiPod.Helpers
+{
+    public static class DispatcherHelper
+    {
+        public static void ThrowIfNotOnUiThread()
+        {
+            var dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+            if(!dispatcher.HasThreadAccess)
+            {
+                throw new ThreadStateException("Not on UI thread");
+            }
+        }
+    }
+}
