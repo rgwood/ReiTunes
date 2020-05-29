24999940c1dd06633f3c18a7002db895a72a7a77
Reilly Wood <reilly.wood@icloud.com>
2020-05-28 21:00:30 -0700

    Add basic download progress monitor

--- ReiTunes/ViewModels/PlayerViewModel.cs
+++ ReiTunes/ViewModels/PlayerViewModel.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Networking.BackgroundTransfer;
@@ -22,6 +23,7 @@ namespace ReiPod
     {
         private IMediaPlaybackSource _source;
         private string _sourceFileName;
+        private string _downloadStatus = "placeholder";
 
         public IMediaPlaybackSource Source
         {
@@ -32,7 +34,7 @@ namespace ReiPod
         public string SourceFileName
         {
             get { return _sourceFileName; }
-            set { Set(ref _sourceFileName, value);  }
+            set { Set(ref _sourceFileName, value); }
         }
 
         private ObservableCollection<FileTreeItem> _fileTreeItems;
@@ -41,6 +43,12 @@ namespace ReiPod
         {
             get { return _fileTreeItems; }
             set { Set(ref _fileTreeItems, value); }
+        }
+
+        public string DownloadStatus
+        {
+            get { return _downloadStatus; }
+            set { Set(ref _downloadStatus, value); }
         }
 
         public PlayerViewModel()
@@ -56,13 +64,13 @@ namespace ReiPod
             var library = await FileHelper.ReiTunesLibrary();
             var libraryFile = await library.TryGetItemAsync("ReiTunesLibrary.txt");
 
-            if(libraryFile == null)
+            if (libraryFile == null)
             {
                 //todo: download it
                 throw new NotImplementedException("Library downloading not ready yet");
             }
 
-            var libraryString = await FileIO.ReadTextAsync((IStorageFile) libraryFile);
+            var libraryString = await FileIO.ReadTextAsync((IStorageFile)libraryFile);
 
             FileTreeItems = FileTreeBuilder.ParseBlobList(libraryString);
         }
@@ -75,29 +83,56 @@ namespace ReiPod
             var musicLib = await FileHelper.ReiTunesLibrary();
             var storageItem = await musicLib.TryGetItemAsync(fileName);
 
-            if(storageItem == null) // file not found, download it
+            if (storageItem == null) // file not found, download it
             {
                 var downloadUri = new Uri(cloudLibraryBaseUri, fileName);
 
                 var downloadFile = await musicLib.CreateFileAsync(fileName);
                 BackgroundDownloader downloader = new BackgroundDownloader();
                 DownloadOperation download = downloader.CreateDownload(downloadUri, downloadFile);
-
-                //TODO: provide some progess indicators while it's downloading
-                await download.StartAsync();
+                Progress<DownloadOperation> progressCallback = new Progress<DownloadOperation>(HandleDownloadProgress);
+                await download.StartAsync().AsTask(progressCallback);
                 storageItem = downloadFile;
             }
 
-            if(storageItem.IsOfType(StorageItemTypes.Folder))
+            if (storageItem.IsOfType(StorageItemTypes.Folder))
             {
                 return;
             }
 
-            if(storageItem.IsOfType(StorageItemTypes.File))
+            if (storageItem.IsOfType(StorageItemTypes.File))
             {
                 Source = MediaSource.CreateFromStorageFile((StorageFile)storageItem);
                 SourceFileName = fileName;
             }
+        }
+
+        private void HandleDownloadProgress(DownloadOperation download)
+        {
+            // DownloadOperation.Progress is updated in real-time while the operation is ongoing. Therefore,
+            // we must make a local copy so that we can have a consistent view of that ever-changing state
+            // throughout this method's lifetime.
+            BackgroundDownloadProgress progress = download.Progress;
+
+            string message = "";
+            if(progress.BytesReceived == progress.TotalBytesToReceive)
+            {
+                message = "Download finished";
+            }
+            else
+            {
+                var kbReceived = progress.BytesReceived / 1024;
+                var totalKb = progress.TotalBytesToReceive / 1024;
+                message = $"{kbReceived} / {totalKb}";
+            }
+
+            // The ignore variable is to silence an async warning. Seems bad but
+            // they did it in the BackgroundTransfer example 🤔
+            var ignore = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal, () =>
+                {
+                    DownloadStatus = message;
+                });
         }
 
     }
