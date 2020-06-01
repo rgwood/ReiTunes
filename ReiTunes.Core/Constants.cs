2a48bdce3ad4320005bb3bf522c4d11fb95e725a
Reilly Wood <reilly.wood@icloud.com>
2020-05-31 22:57:03 -0700

    Create Azure Function for updating library

---
+++ ReiTunes.Core/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReiTunes.Core
+{
+    public static class Constants
+    {
+        public const string MusicContainerName = "music";
+        public const string LibraryContainerName = "library";
+        public const string LibraryFileName = "ReiTunesLibrary.txt";
+    }
+}
