c99ddc30197a50e40814ec639f74e0738429ce3e
Reilly Wood <reilly.wood@icloud.com>
2020-06-12 21:40:42 -0700

    add GUID and fix deserialization

--- ReiTunes.Core/Models/LibraryItem.cs
+++ ReiTunes.Core/Models/LibraryItem.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using ReiTunes.Core;
 
 namespace ReiTunes.Core {
@@ -19,9 +21,23 @@ namespace ReiTunes.Core {
             set { Set(ref _fullPath, value); }
         }
 
-        public LibraryItem(string fileName, string fullPath) {
-            _name = fileName;
-            _fullPath = fullPath;
+        private Guid _id;
+
+        public Guid Id {
+            get { return _id; }
+            set { Set(ref _id, value); }
         }
+
+        //for deserialization
+        public LibraryItem() {
+        }
+
+        public LibraryItem(string relativePath) {
+            _id = Guid.NewGuid();
+            _name = GetFileNameFromPath(relativePath);
+            _fullPath = relativePath;
+        }
+
+        private string GetFileNameFromPath(string path) => path.Split('/').Last();
     }
 }
