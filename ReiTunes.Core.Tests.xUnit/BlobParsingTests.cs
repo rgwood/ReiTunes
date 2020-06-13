1ef7f973daba8c43d5ea76a42ab68c24415fcedb
Reilly Wood <reilly.wood@icloud.com>
2020-06-12 21:00:52 -0700

    start adopting new brace formatting

--- ReiTunes.Core.Tests.xUnit/BlobParsingTests.cs
+++ ReiTunes.Core.Tests.xUnit/BlobParsingTests.cs
@@ -1,18 +1,17 @@
 using ReiTunes.Core;
 using Xunit;
 
-namespace ReiTunes.Test
-{
-    public class BlobParsingTests
-    {
+namespace ReiTunes.Test {
+
+    public class BlobParsingTests {
+
         private string rawBlobList = @"Blah/DJ Set.mp3
 Blah/DJ Set 2.mp3
 DJ sdofkmgokm.m4a
 ddoklmfglkm.mp3";
 
         [Fact]
-        public void CanParseBlobList()
-        {
+        public void CanParseBlobList() {
             var explorerItems = LibraryFileParser.ParseBlobList(rawBlobList);
             Assert.Equal(4, explorerItems.Count);
         }
--- ReiTunes.Core/Json.cs
+++ ReiTunes.Core/Json.cs
@@ -1,22 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System.Threading.Tasks;
 
-namespace ReiTunes.Core
-{
-    public static class Json
-    {
-        public static async Task<T> ToObjectAsync<T>(string value)
-        {
-            return await Task.Run<T>(() =>
-            {
+namespace ReiTunes.Core {
+
+    public static class Json {
+
+        public static async Task<T> ToObjectAsync<T>(string value) {
+            return await Task.Run<T>(() => {
                 return JsonConvert.DeserializeObject<T>(value);
             });
         }
 
-        public static async Task<string> StringifyAsync(object value)
-        {
-            return await Task.Run<string>(() =>
-            {
+        public static async Task<string> StringifyAsync(object value) {
+            return await Task.Run<string>(() => {
                 // Pretty-print for convenience. Revisit this if it ever becomes
                 // a perf issue, but for now YAGNI
                 return JsonConvert.SerializeObject(value, Formatting.Indented);
