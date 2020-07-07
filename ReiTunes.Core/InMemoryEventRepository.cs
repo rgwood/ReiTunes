a96a8761970c913f8f1f47d8b0f5b38f4cda5456
Reilly Wood <reilly.wood@icloud.com>
2020-07-06 20:00:11 -0700

    Add repo functions to check event existence and get all events

--- ReiTunes.Core/InMemoryEventRepository.cs
+++ ReiTunes.Core/InMemoryEventRepository.cs
@@ -8,11 +8,22 @@ namespace ReiTunes.Core {
     public class InMemoryEventRepository : IEventRepository {
         private List<IEvent> _events = new List<IEvent>();
 
+        public bool ContainsEvent(Guid eventId) {
+            return _events.Any(e => e.Id == eventId);
+        }
+
+        public IEnumerable<IEvent> GetAllEvents() {
+            return _events;
+        }
+
         public IEnumerable<IEvent> GetEvents(Guid aggregateId) {
             return _events.Where(e => e.AggregateId == aggregateId);
         }
 
         public void Save(IEvent @event) {
+            if (ContainsEvent(@event.Id))
+                return;
+
             _events.Add(@event);
         }
     }
