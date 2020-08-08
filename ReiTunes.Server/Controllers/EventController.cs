e23db500e796ecaee145ca7e77f1fe25264c402b
Reilly Wood <reilly.wood@icloud.com>
2020-08-08 11:04:49 -0700

    Add server  with some basic integration tests

---
+++ ReiTunes.Server/Controllers/EventController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ReiTunes.Server.Controllers {
+
+    [ApiController]
+    [Route("events")]
+    public class EventController : ControllerBase {
+        private readonly ILogger<EventController> _logger;
+
+        public EventController(ILogger<EventController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new List<string>() { "foo", "bar" };
+        }
+
+        [HttpPut]
+        [Route("exclaim")]
+        public string Exclaim(string input) {
+            return input + "!";
+        }
+    }
+}
