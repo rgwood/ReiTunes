90b78756d745e2dd9f1688bf0b0512e4b061bf8c
Reilly Wood <reilly.wood@icloud.com>
2020-08-08 13:43:44 -0700

    Add in-memory helper

--- ReiTunes.Core/SQLiteHelpers.cs
+++ ReiTunes.Core/SQLiteHelpers.cs
@@ -3,10 +3,16 @@ using System.Data.SQLite;
 
 namespace ReiTunes.Core {
 
-    public static class SQLiteExtensions {
+    public static class SQLiteHelpers {
 
         public static long GetRowCount(this SQLiteConnection conn, string tableName) {
             return conn.QuerySingle<long>($"SELECT COUNT() FROM {tableName}");
+        }
+
+        public static SQLiteConnection CreateInMemoryDb() {
+            var connection = new SQLiteConnection("DataSource=:memory:");
+            connection.Open();
+            return connection;
         }
     }
 }
