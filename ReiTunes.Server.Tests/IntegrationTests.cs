23501fe8af015aace0ed655f5b0059dfe8f64943
Reilly Wood <reilly.wood@icloud.com>
2020-08-08 13:44:43 -0700

    Wire up SQLite to EventController

--- ReiTunes.Server.Tests/IntegrationTests.cs
+++ ReiTunes.Server.Tests/IntegrationTests.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.WebUtilities;
 using Newtonsoft.Json;
+using ReiTunes.Core;
 using ReiTunes.Server.Controllers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +13,56 @@ using Xunit;
 
 namespace ReiTunes.Server.Tests {
 
-    public class IntegrationTests : IClassFixture<WebApplicationFactory<ReiTunes.Server.Startup>> {
-        private readonly WebApplicationFactory<Startup> _factory;
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<InMemoryStartup>> {
+        private readonly WebApplicationFactory<InMemoryStartup> _factory;
 
-        public IntegrationTests(WebApplicationFactory<Startup> factory) {
+        public IntegrationTests(WebApplicationFactory<InMemoryStartup> factory) {
             _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanSaveAndRetrieveSingleEvent() {
+            var client = _factory.CreateClient();
+            List<string> serialized = await GetAllSerializedEventsAsync(client);
+
+            Assert.Empty(serialized);
+
+            var agg = new SimpleTextAggregate("foo");
+            var @event = agg.GetUncommittedEvents().Single();
+            @event.MachineName = "Cornelius";
+            await SaveEventAsync(client, @event);
+
+            serialized = await GetAllSerializedEventsAsync(client);
+
+            var deserializedEvent = await EventSerialization.DeserializeAsync(serialized.Single());
+            AssertEventsAreEqual(@event, deserializedEvent);
+        }
+
+        private static async Task SaveEventAsync(HttpClient client, IEvent @event) {
+            var putUri = QueryHelpers.AddQueryString("/events/save", "serializedEvent", EventSerialization.Serialize(@event));
+
+            var putResponse = await client.PutAsync(putUri, null);
+
+            putResponse.EnsureSuccessStatusCode();
+        }
+
+        // TODO: should add better equality comparers to the event classes themselves
+        private static void AssertEventsAreEqual(IEvent event1, IEvent event2) {
+            Assert.Equal(event1.Id, event2.Id);
+            Assert.Equal(event1.AggregateId, event2.AggregateId);
+            Assert.Equal(event1.CreatedTimeUtc, event2.CreatedTimeUtc);
+            Assert.Equal(event1.MachineName, event2.MachineName);
+            Assert.Equal(event1.GetType(), event2.GetType());
+        }
+
+        private static async Task<List<string>> GetAllSerializedEventsAsync(HttpClient client) {
+            var response = await client.GetAsync("/events");
+            response.EnsureSuccessStatusCode();
+
+            var contents = await response.Content.ReadAsStringAsync();
+
+            var deserialized = await Json.DeserializeAsync<List<string>>(contents);
+            return deserialized;
         }
 
         [Fact]
@@ -42,6 +89,23 @@ namespace ReiTunes.Server.Tests {
             var contents = await response.Content.ReadAsStringAsync();
 
             Assert.Equal("foo!", contents);
+        }
+
+        [Fact]
+        public async Task TestControllerGetEnumerable() {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/test/enumerable");
+
+            response.EnsureSuccessStatusCode();
+
+            var contents = await response.Content.ReadAsStringAsync();
+
+            var deserialized = JsonConvert.DeserializeObject<List<string>>(contents);
+
+            Assert.Equal(2, deserialized.Count());
+            Assert.Equal(TestController.GoodString, deserialized[0]);
+            Assert.Equal(TestController.BadString, deserialized[1]);
         }
 
         [Fact]
