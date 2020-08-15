a13d13788a8078acbc89df45413713d1f7f56d96
Reilly Wood <reilly.wood@icloud.com>
2020-08-15 16:06:21 -0700

    reformat

--- ReiTunes/Helpers/FileHelper.cs
+++ ReiTunes/Helpers/FileHelper.cs
@@ -2,14 +2,12 @@
 using System.Threading.Tasks;
 using Windows.Storage;
 
-namespace ReiTunes.Helpers
-{
-    public class FileHelper
-    {
+namespace ReiTunes.Helpers {
+
+    public class FileHelper {
         public const string LibraryFolderName = "ReiTunes";
 
-        public async static Task<StorageFolder> CreateLibraryFolderIfDoesntExist()
-        {
+        public async static Task<StorageFolder> CreateLibraryFolderIfDoesntExist() {
             var musicLibrary = KnownFolders.MusicLibrary;
             return await musicLibrary.CreateFolderAsync(LibraryFolderName, CreationCollisionOption.OpenIfExists);
         }
