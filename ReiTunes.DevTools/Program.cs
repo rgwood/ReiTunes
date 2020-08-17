3c0e8530c4c1ebf197205155936acaf3de5a905e
Reilly Wood <reilly.wood@icloud.com>
2020-08-16 21:02:35 -0700

    tool to add lots of items at once

--- ReiTunes.DevTools/Program.cs
+++ ReiTunes.DevTools/Program.cs
@@ -1,17 +1,39 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using ReiTunes.Core;
+using Serilog.Core;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReiTunes.DevTools
-{
-    internal class Program
-    {
-        private static async Task Main(string[] args)
-        {
+namespace ReiTunes.DevTools {
+
+    internal class Program {
+
+        private static async Task Main(string[] args) {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("PLACEHOLDER");
+
+            var caller = new ServerCaller(client, Logger.None);
+
+            var sw = Stopwatch.StartNew();
+
+            foreach (var item in items.Split(Environment.NewLine)) {
+                Console.WriteLine(item);
+
+                await caller.CreateNewLibraryItemAsync(item);
+            }
+
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms");
+        }
+
+        private static string items = @"Avalanches/01 DJ Set - Brains Party @ St Jerome.mp3
+Avalanches/02 BeatsInSpace-04.01.14 Part2 with.mp3";
+
+        private static async Task GenerateLibraryFromBlobStorage() {
             // setx AZURE_STORAGE_CONNECTION_STRING "<yourconnectionstring>"
             var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
             var library = await GenerateLibrary(connectionString);
@@ -21,34 +43,29 @@ namespace ReiTunes.DevTools
             Console.WriteLine("finished writing");
         }
 
-        private static async Task<string> GenerateLibrary(string connectionString)
-        {
+        private static async Task<string> GenerateLibrary(string connectionString) {
             var ret = new StringBuilder();
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(Constants.MusicContainerName);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(Core.Constants.MusicContainerName);
 
-            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
-            {
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync()) {
                 ret.AppendLine(blobItem.Name);
             }
 
             return ret.ToString();
         }
 
-        private static async Task WriteLibrary(string connectionString, string libraryContents)
-        {
+        private static async Task WriteLibrary(string connectionString, string libraryContents) {
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(Constants.LibraryContainerName);
-            BlobClient blobClient = containerClient.GetBlobClient(Constants.LibraryFileName);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(Core.Constants.LibraryContainerName);
+            BlobClient blobClient = containerClient.GetBlobClient(Core.Constants.LibraryFileName);
 
-            using (var libraryContentsStream = GenerateStreamFromString(libraryContents))
-            {
+            using (var libraryContentsStream = GenerateStreamFromString(libraryContents)) {
                 await blobClient.UploadAsync(libraryContentsStream, true);
             }
         }
 
-        public static Stream GenerateStreamFromString(string s)
-        {
+        public static Stream GenerateStreamFromString(string s) {
             var stream = new MemoryStream();
             var writer = new StreamWriter(stream);
             writer.Write(s);
