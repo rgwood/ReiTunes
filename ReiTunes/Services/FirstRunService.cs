7aeaf22c7a4b8164a7fafb4cdc2ff56f1cfda71f
Reilly Wood <reilly.wood@icloud.com>
2020-09-12 16:12:48 -0700

    Pull events automatically on first app load

---
+++ ReiTunes/Services/FirstRunService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using ReiTunes.Configuration;
+using ReiTunes.Core;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace ReiTunes.Services {
+
+    public static class FirstRunService {
+        private static bool shown = false;
+
+        internal static async Task ShowIfAppropriateAsync() {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal, async () => {
+                    if (SystemInformation.IsFirstRun && !shown) {
+                        // pull events in for the first time
+                        await ServiceLocator.Current.GetService<PlayerViewModel>().PullEventsCommand.ExecuteAsync(null);
+                        shown = true;
+                        //var dialog = new FirstRunDialog();
+                        //await dialog.ShowAsync();
+                    }
+                });
+        }
+    }
+}
