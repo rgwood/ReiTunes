d927694ce58fa3efac28b210cb2b4cb6ea77bd8d
Reilly Wood <reilly.wood@icloud.com>
2020-11-30 00:30:25 -0800

    Naming and config

--- ReiTunes.Core.Tests.xUnit/JsonSnapshotTests.cs
+++ ReiTunes.Core.Tests.xUnit/JsonSnapshotTests.cs
@@ -9,12 +9,10 @@ using ApprovalTests.Reporters.ContinuousIntegration;
 
 namespace ReiTunes.Test {
 
-    [UseReporter(typeof(BeyondCompareReporter))]
     public class JsonSnapshotTests {
 
         private const string MachineName = "Cornelius";
         private static readonly Guid _item1ID = new("47b04010-5bc0-410e-9803-f5d6f3c5badc");
-        private static readonly Guid _item2ID = new("a8fd3845-7b54-4a72-a818-bb7af3c6d3cc");
         private readonly LibraryItemEventFactory _factory = new(
             new NeverIncreasingClock(),
             MachineName,
@@ -37,15 +35,5 @@ namespace ReiTunes.Test {
         public void ItemNameChangedEvent() =>
             VerifyJson(_factory.GetNameChangedEvent(_item1ID, "newName"));
 
-        //Disabled because we no longer need to serialize aggregates - we instead serialize their events
-        //[Fact]
-        //public async void CanSerializeAndDeserializeSampleData() {
-        //    ObservableCollection<LibraryItem> sampleData = LibraryFileParser.GetSampleData();
-
-        //    var serialized = await Json.StringifyAsync(sampleData);
-        //    var deserialized = await Json.ToObjectAsync<List<LibraryItem>>(serialized);
-
-        //    Assert.Equal(sampleData.Count, deserialized.Count);
-        //}
     }
 }
