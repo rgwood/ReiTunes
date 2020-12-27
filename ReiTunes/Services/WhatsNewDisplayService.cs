ce1cd84e95600ecc14e283f255617c0da9684011
Reilly Wood <reilly.wood@icloud.com>
2020-12-27 11:01:10 -0800

    Add TODO

--- ReiTunes/Services/WhatsNewDisplayService.cs
+++ ReiTunes/Services/WhatsNewDisplayService.cs
@@ -6,27 +6,24 @@ using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 
-namespace ReiTunes.Services
-{
+namespace ReiTunes.Services {
+
     // For instructions on testing this service see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/UWP/features/whats-new-prompt.md
-    public static class WhatsNewDisplayService
-    {
+    public static class WhatsNewDisplayService {
         private static bool shown = false;
 
-        internal static async Task ShowIfAppropriateAsync()
-        {
+        internal static async Task ShowIfAppropriateAsync() {
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
-                CoreDispatcherPriority.Normal, async () =>
-                {
-                    if (SystemInformation.IsAppUpdated && !shown)
-                    {
+                CoreDispatcherPriority.Normal, async () => {
+                    if (SystemInformation.IsAppUpdated && !shown) {
                         shown = true;
                         var ver = Package.Current.Id.Version;
 
+                        // TODO: move this into XAML and set the background colour appropriately
                         ContentDialog dialog = new ContentDialog() {
                             Title = "ReiTunes Was Updated",
                             Content = $"Now on version {ver.Major}.{ver.Minor}.{ver.Build}.{ver.Revision}",
-                            CloseButtonText= "Cool"
+                            CloseButtonText = "Cool"
                         };
                         await dialog.ShowAsync();
                     }
