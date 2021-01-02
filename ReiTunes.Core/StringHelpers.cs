842f81e697c0c4981969754d5dcf710cb7cc356c
Reilly Wood <reilly.wood@icloud.com>
2021-01-01 18:53:57 -0800

    Add text element/rune helper for working with emoji

---
+++ ReiTunes.Core/StringHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ReiTunes.Core {
+
+    public static class StringHelpers {
+
+        // Use Rune instead of this once we're able to be on .NET Core 3.1 and above
+        public static List<string> TextElements(this string input) {
+            var enumerator = System.Globalization.StringInfo.GetTextElementEnumerator(input);
+            var list = new List<string>();
+            while (enumerator.MoveNext()) {
+                list.Add(enumerator.GetTextElement());
+            }
+            return list;
+        }
+    }
+}
