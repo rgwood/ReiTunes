f3c3d266eb4535e22e96add615f8b3097ec8ff34
reilly.wood <reilly.wood@icloud.com>
2021-09-25 20:38:49 -0700

    Add ReiTunesFzf project

---
+++ ReiTunesFzf/Program.cs
@@ -0,0 +1,14 @@
+﻿using ReiTunes.Core;
+
+// TODO find a way to get the packaged app path...
+// this is not the normal location of the library.db file
+string libraryPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Music\library.db");
+SQLiteEventRepository repo = new(SQLiteHelpers.CreateFileDb(libraryPath));
+Library lib = new(SQLiteHelpers.CreateFileDb(libraryPath), LoggerHelpers.DoNothingLogger());
+
+foreach (LibraryItem item in lib.Items) {
+    if(string.IsNullOrEmpty(item.Album))
+        Console.WriteLine($"{item.AggregateId}\t{item.Name} - {item.Artist}");
+    else
+        Console.WriteLine($"{item.AggregateId}\t{item.Name} - {item.Artist} - {item.Album}");
+}
---
+++ ReiTunesFzf/README.md
@@ -0,0 +1,17 @@
+﻿# ReiTunesFzf
+
+A little CLI tool to spit out lines in a format suitable for fzf to consume. For example:
+
+```
+reitunes.exe (ReiTunesFzf.exe | fzf --delimiter='\t' --with-nth=2)
+```
+
+This opens up fzf with a list of all songs in the local ReiTunes library and then opens the chosen one in ReiTunes.
+
+## TODO
+
+Figure out how to get the full path of the local appdata folder. library.db is normally stored here:
+
+> AppData\Local\Packages\2b3c53fd-b804-4e1e-b26c-cae302ea1108_hmppzwgz807yp\LocalState
+
+Not clear how to get that full path in a way that will work across machines and installs.
