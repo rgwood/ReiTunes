7ed48d47e663c4f2dcfc32bb766526aaeeaf418b
reilly.wood <reilly.wood@icloud.com>
2021-09-25 21:00:40 -0700

    Fix library.db path in ReiTunesFzf

--- ReiTunesFzf/Program.cs
+++ ReiTunesFzf/Program.cs
@@ -1,8 +1,7 @@
 ﻿using ReiTunes.Core;
 
-// TODO find a way to get the packaged app path...
-// this is not the normal location of the library.db file
-string libraryPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Music\library.db");
+// TODO can we calculate 2b3c53fd-b804-4e1e-b26c-cae302ea1108_hmppzwgz807yp instead of hardcoding it here?
+string libraryPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\Packages\2b3c53fd-b804-4e1e-b26c-cae302ea1108_hmppzwgz807yp\LocalState\library.db");
 SQLiteEventRepository repo = new(SQLiteHelpers.CreateFileDb(libraryPath));
 Library lib = new(SQLiteHelpers.CreateFileDb(libraryPath), LoggerHelpers.DoNothingLogger());
 
