3789e5705e0e12abd027131716407e77a7ac3918
Reilly Wood <reilly.wood@icloud.com>
2021-11-08 17:41:17 -0800

    Fix fzf

--- ReiTunesFzf/Program.cs
+++ ReiTunesFzf/Program.cs
@@ -3,7 +3,7 @@
 // TODO can we calculate 2b3c53fd-b804-4e1e-b26c-cae302ea1108_hmppzwgz807yp instead of hardcoding it here?
 string libraryPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\Packages\2b3c53fd-b804-4e1e-b26c-cae302ea1108_hmppzwgz807yp\LocalState\library.db");
 SQLiteEventRepository repo = new(SQLiteHelpers.CreateFileDb(libraryPath));
-Library lib = new(SQLiteHelpers.CreateFileDb(libraryPath), LoggerHelpers.DoNothingLogger());
+Library lib = new(SQLiteHelpers.CreateFileDb(libraryPath), LoggerHelpers.DoNothingLogger(), new NoopServerCaller());
 
 foreach (LibraryItem item in lib.Items) {
     if(string.IsNullOrEmpty(item.Album))
