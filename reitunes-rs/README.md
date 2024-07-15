f60287d271ad9b94dd1ef3618c0600df361a2a01
Reilly Wood <reilly.wood@icloud.com>
2024-07-15 16:42:05 -0700

    More Rust work

--- reitunes-rs/README.md
+++ reitunes-rs/README.md
@@ -4,7 +4,15 @@ Everything in here is extremely WIP.
 
 ## TODO
 
+- [x] Read a row from the DB into a struct w/ a string Serialized column
+- [ ] Make an event enum
 - [ ] Figure out how to map the events table rows to Rust idiomatically. Can't really do interfaces like in C#
 - [ ] Create LibraryItem and Bookmark structs
 - [ ] Write some tests for applying events to a single node
 - [ ] Port C# tests for multiple nodes / syncing
+
+## Thoughts
+
+There's a ton of super redundant data in the Serialized column... how far do I want to go to maintain backwards compatibility?
+If I redesign things I will need to replace *everything* in C#. Server and serverless function. But maybe that's OK.
+Leaning toward redesigning and simplifying. Ignore unused fields in the serialized column.
--- reitunes-rs/src/main.rs
+++ reitunes-rs/src/main.rs
@@ -1,6 +1,41 @@
-use anyhow::Result;
+use std::time::SystemTime;
+
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+use serde_rusqlite::*;
+use time::{OffsetDateTime, PrimitiveDateTime};
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+
+    let conn = rusqlite::Connection::open("test-library.db")?;
+    let mut stmt = conn.prepare_cached("SELECT * FROM events LIMIT 1")?;
+
+    let event = from_rows::<EventRow>(stmt.query([])?)
+        .next()
+        .context(format!("Event not found"))??;
+
+    println!("{:?}", event);
+
     Ok(())
 }
+// Id                                  │AggregateId                         │AggregateType│CreatedTimeUtc             │MachineName│Serialized
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct EventRow {
+    id: uuid::Uuid,
+    aggregate_id: uuid::Uuid,
+    aggregate_type: String,
+    created_time_utc: PrimitiveDateTime,
+    machine_name: String,
+    serialized: String,
+}
+
+// {"$type":"LibraryItemPlayedEvent","Id":"ba6f6676-9c39-4262-b69a-1433b3b43255","AggregateId":"559146d5-4901-4e09-abd9-e732a23f8429","CreatedTimeUtc":"2020-08-15T22:52:09.8397077Z","LocalId":1,"MachineName":"SURFACESPUD"}
+
+// pub struct LibraryItemPlayedEvent {
+//     id: Uuid,
+//     aggregate_id: Uuid,
+//     created_time_utc: DateTime<Utc>,
+//     local_id: i32,
+//     machine_name: String,
+// }
