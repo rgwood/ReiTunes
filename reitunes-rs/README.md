8d04f5abc9a34946cd847a931cec377672dddb92
Reilly Wood <reilly.wood@icloud.com>
2024-08-04 18:29:55 -0700

    Rust progress

--- reitunes-rs/README.md
+++ reitunes-rs/README.md
@@ -16,3 +16,12 @@ Everything in here is extremely WIP.
 There's a ton of super redundant data in the Serialized column... how far do I want to go to maintain backwards compatibility?
 If I redesign things I will need to replace *everything* in C#. Server and serverless function. But maybe that's OK.
 Leaning toward redesigning and simplifying. Ignore unused fields in the serialized column.
+
+
+# Sunday plan of attack
+
+- Implement LibraryItemCreated and LibraryItemPlayed
+    - Add an apply() fn
+    - Add a struct for LibraryItem
+- Get Aider to implement the rest
+- Get Aider to write a bunch of tests
--- reitunes-rs/src/main.rs
+++ reitunes-rs/src/main.rs
@@ -1,25 +1,32 @@
-use std::time::SystemTime;
+use std::{cell::{LazyCell, OnceCell}, sync::LazyLock, vec};
 
 use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
 use serde_rusqlite::*;
+// TODO: consider using jiff as a time library
 use time::{OffsetDateTime, PrimitiveDateTime};
 
 fn main() -> Result<()> {
 
+    let mut library: Vec<LibraryItem> = vec![];
+
     let conn = rusqlite::Connection::open("test-library.db")?;
-    let mut stmt = conn.prepare_cached("SELECT * FROM events LIMIT 1")?;
+    let mut stmt = conn.prepare_cached("SELECT * FROM events ORDER BY CreatedTimeUtc LIMIT 2")?;
 
-    let event = from_rows::<EventRow>(stmt.query([])?)
-        .next()
-        .context(format!("Event not found"))??;
+    let rows = from_rows::<EventRow>(stmt.query([])?);
 
-    println!("{:?}", event);
+    for row in rows {
+        let row = row?;
+        println!("{:?}", row.serialized);
+        let event = row.event()?;
+        println!("{:?}", event);
+    }
 
     Ok(())
 }
+
 // Id                                  │AggregateId                         │AggregateType│CreatedTimeUtc             │MachineName│Serialized
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct EventRow {
     id: uuid::Uuid,
@@ -28,14 +35,60 @@ pub struct EventRow {
     created_time_utc: PrimitiveDateTime,
     machine_name: String,
     serialized: String,
+    #[serde(skip)]
+    event: LazyLock<Event>
 }
 
-// {"$type":"LibraryItemPlayedEvent","Id":"ba6f6676-9c39-4262-b69a-1433b3b43255","AggregateId":"559146d5-4901-4e09-abd9-e732a23f8429","CreatedTimeUtc":"2020-08-15T22:52:09.8397077Z","LocalId":1,"MachineName":"SURFACESPUD"}
+impl EventRow {
+    pub fn event(&mut self) -> Result<Event> {
+        if let Some(event) = self.event.get() {
+            return Ok(event.clone());
+        }
+        let event: Event = serde_json::from_str(&self.serialized)
+            .context("Failed to deserialize event")?;
+        Ok(event)
+    }
+}
 
-// pub struct LibraryItemPlayedEvent {
-//     id: Uuid,
-//     aggregate_id: Uuid,
-//     created_time_utc: DateTime<Utc>,
-//     local_id: i32,
-//     machine_name: String,
-// }
+
+
+pub fn apply(row: EventRow, library: &mut Vec<LibraryItem>) {
+    match event {
+        Event::LibraryItemCreatedEvent { name, file_path } => {
+            library.push(LibraryItem {
+                id: uuid::Uuid::new_v4(),
+                name,
+                file_path,
+            });
+        }
+        _ => {}
+    }
+}
+
+pub struct EventWithRow {
+    event: Event,
+    row: EventRow,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
+#[serde(tag = "$type")]
+pub enum Event {
+    #[default]
+    LibraryItemPlayedEvent,
+    #[serde(rename_all = "PascalCase")]
+    LibraryItemCreatedEvent{
+        name: String,
+        file_path: String,
+    },
+    // TODO: Add more events
+}
+
+pub struct LibraryItem {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub file_path: String,
+}
+
+
+
+// {"$type":"LibraryItemPlayedEvent","Id":"ba6f6676-9c39-4262-b69a-1433b3b43255","AggregateId":"559146d5-4901-4e09-abd9-e732a23f8429","CreatedTimeUtc":"2020-08-15T22:52:09.8397077Z","LocalId":1,"MachineName":"SURFACESPUD"}
