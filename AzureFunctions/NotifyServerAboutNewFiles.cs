628fa00881232008e5d67b7a916a95d7e82a6d56
Reilly Wood <reilly.wood@icloud.com>
2024-09-14 18:18:17 -0700

    Finish implementing new Azure function

--- AzureFunctions/NotifyServerAboutNewFiles.cs
+++ AzureFunctions/NotifyServerAboutNewFiles.cs
@@ -2,6 +2,7 @@
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
 using System;
+using System.Net.Http.Json;
 using Azure.Messaging;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
@@ -20,7 +21,7 @@ public class NotifyServerAboutNewFiles
     const string MusicContainerName = "music";
 
     [Function(nameof(NotifyServerAboutNewFiles))]
-    public void Run([EventGridTrigger] CloudEvent cloudEvent)
+    public async Task Run([EventGridTrigger] CloudEvent cloudEvent)
     {
         _logger.LogInformation("Event type: {type}, Event subject: {subject}", cloudEvent.Type, cloudEvent.Subject);
         _logger.LogInformation(cloudEvent.Data.ToString());
@@ -33,8 +34,13 @@ public class NotifyServerAboutNewFiles
             string? filePath = string.Join('/', tokens.Skip(6));
             _logger.LogInformation($"Change detected in container {container} for file '{filePath}'");
 
-            // TODO: make HTTP call with API key to notify server about new file
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", Environment.GetEnvironmentVariable("REITUNES_API_KEY"));
 
+            var content = JsonContent.Create(new { file_path = filePath });
+            var response = await client.PostAsync($"{Environment.GetEnvironmentVariable("REITUNES_HOSTNAME")}/api/add", content);
+            _logger.LogInformation($"Response: {response.StatusCode}");
+            response.EnsureSuccessStatusCode();
         }
         else
         {
